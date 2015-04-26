13d0226e545bcdacead9c0205d174fa110a4721c
Akshay Singnurkar <akshaysingnurkar@gmail.com>
2015-04-26 17:58:01 -0400

    40%

--- public/main.js
+++ public/main.js
@@ -1,9 +1,5 @@
 (function($) {
 
-	$(window).on('scroll', function() {
-		console.log('scroll')
-	})
-
 	$(window).scroll(function() {
 		var yPos = -($(window).scrollTop() / 15); 
 		var bgpos = '50% '+ yPos + 'px';
@@ -13,5 +9,27 @@
 		var bgpos2 = '50% '+ yPos2 + 'px';
 		$('.container').css('background-position', bgpos2 );
 	}); 
+
+	// var $workGrid = $('#work-grid')
+
+	// $workGrid
+	// 	.children('.grid-item')
+	// 	.on('mouseenter', function(e) {
+	// 		$(this).children('.work-overlay').fadeIn(250)
+	// 	})		
+
+	// $workGrid
+	// 	.children('.grid-item')
+	// 	$(e.target).on('mouseleave', function(e) {
+	// 	$(e.target).fadeOut(250)
+	// })
+
+	$('.grid-item')
+		.on('mouseenter', function(e) {
+			$(this).find('.work-overlay').fadeIn(250)
+		})
+		.on('mouseleave', function(e) {
+			$(this).find('.work-overlay').fadeOut(250)
+		})
 
 })(jQuery);
