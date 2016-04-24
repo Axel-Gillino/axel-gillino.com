f142daababc34f78a79aed2583a7e33f2077afaf
Shay Sing <hi@shay.tech>
2016-04-24 16:23:04 -0400

    clients

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,8 @@
 (function() {
     'use strict'
-
+    
+    new WOW().init();
+    
     $('.client').on('mouseenter', function (e) {
         var bgColor = $(e.target).data('bg-color');
         var fgColor = $(e.target).data('fg-color');
@@ -8,25 +10,22 @@
         $(this).css('color', fgColor);
     });
 
-    $('a').on('click', function(evt) {
-        ga('send', 'event', 'Link', null, $(evt.target).attr('href'), null)
+    $('a').on('click', function(e) {
+        ga('send', 'event', 'Link', null, $(e.target).attr('href'), null)
     });
 
-    $('.slide')
-        .on('mouseenter', function(e) { $(this).find('.slide-overlay').fadeIn(250) })  
-        .on('mouseleave', function(e) { $(this).find('.slide-overlay').fadeOut(250) })
-
-    var overlay = $('.overlay')
-    var toggleOverlay = function () {
+    var overlay = $('.overlay');
+    var toggleOverlay = function() {
         if (overlay.hasClass('open')) {
             overlay.removeClass('open');
             overlay.addClass('close');
             return
-        } 
+        }
         overlay.addClass('open');
         overlay.removeClass('close');
     }
 
     $('#trigger-overlay').on('click', toggleOverlay);
     $('.overlay-close').on('click', toggleOverlay);
+    
 })();
